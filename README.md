80c3b453ee42055eacc26d9efb7a18570a19eff6
Tim0727 <timlam727@gmail.com>
2024-05-04 23:47:19 -0700

    testing branch: should be wrong

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Lab 5 - Starter
 Wai Taing Lam
+
+1) I would not. This is because unit testing is primarily focused on testing individual components. However, the "message" feature involves multiple components working together.
+2) Yes, I would. The "max message length" feature is an individual component of a messaging application ensuring the message does not exceed a specific length (80 characters limit). We can write a unit test to verify that the application correctly follows the 80 characters limit.
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,56 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const synth = window.speechSynthesis;
+
+  const smileImage = document.querySelector('img');
+  const voiceSelect = document.getElementById('voice-select');
+  const textInput = document.getElementById('text-to-speak');
+  const talkButton = document.querySelector('button');
+
+  const smilingFace = 'assets/images/smiling.png';
+  const smilingOpenFace = 'assets/images/smiling-open.png';
+
+  let voices = [];
+
+  function populateVoiceList() {
+    voices = synth.getVoices();
+  
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+  
+      if (voices[i].default) {
+        option.textContent += " — DEFAULT";
+      }
+  
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+      voiceSelect.appendChild(option);
+    }
+  }
+  
+  populateVoiceList();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+
+  function speak() {
+    const utterThis = new SpeechSynthesisUtterance(textInput.value);
+    const selectedOption = voiceSelect.selectedOptions[0].getAttribute("data-name");
+    for (let i = 0; i < voices.length; i++) {
+      if (voices[i].name === selectedOption) {
+        utterThis.voice = voices[i];
+      }
+    }
+    speechSynthesis.speak(utterThis);
+    smileImage.src = smilingOpenFace;
+    utterThis.onend = () => {
+      smileImage.src = smilingFace;
+    };
+  
+    textInput.blur();
+  }
+
+  talkButton.addEventListener('click', speak);
 }
